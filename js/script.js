df85567ff711cea841ba13b089dc60da04a3eda9
Alvaro2005 <78221033+Alvaro2005@users.noreply.github.com>
2021-07-11 13:28:50 -0400

    los efectos ya puestos

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ window.addEventListener("scroll", () => {
 
         for (let i = 0; i < imagen.length; i++) {
 
-            let altura = window.innerHeight / 3;
+            let altura = window.innerHeight / 1.2;
 
             let distancia = imagen[i].getBoundingClientRect().top;
 
